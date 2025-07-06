3cb32e4810eca3056f909812b77131bd2a674907
Oleksii Milevskyi <123121237+omilevskyi@users.noreply.github.com>
2025-07-06 23:22:40 +0300

    Add first file set (#1)

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/omilevskyi/obsolete-packages
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+const (
+	appName = "obsolete-packages"
+)
+
+var (
+	version   = "0.0.1"
+	verbosity int
+)
+
+func main() {
+}
